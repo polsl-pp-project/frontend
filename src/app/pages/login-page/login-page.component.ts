485f09315a1f09f97a476b98f8cf900e4b6b7e57
nowak2 <piotr.nowakowski9@gmail.com>
2023-04-17 19:17:04 +0200

    Login Done

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
-
+import { Login } from 'src/app/models/login'
+ 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -10,4 +10,31 @@ import { AuthService } from 'src/app/services/auth-service/auth.service';
 })
 export class LoginPageComponent {
 
-}
+  submitted = false;
+
+  constructor(private authService: AuthService,
+    private fb: FormBuilder) { }
+
+  get f(): {
+    [key: string]: AbstractControl;
+  } { return this.form.controls; }
+
+  form: FormGroup = this.fb.group({
+
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+    
+  })
+  
+
+  login() {
+    this.submitted = true;
+     let login: Login = this.form.getRawValue();
+      this.authService.login(login).subscribe((response) => {
+        console.log(response);
+        
+      })
+    }
+  }
+
+
--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { Login } from 'src/app/models/login';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,9 @@ export class AuthService {
 
   signup(user: User) {
     return this.httpService.post<User>("/api/v1/users/signup", user, {});
+  }
+
+  login(login: Login) {
+    return this.httpService.post<Login>("/api/v1/users/login", login, {});
   }
 }
