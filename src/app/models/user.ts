959f6cd66cb861b788a30801fbd8f15c402bdb41
nowak2 <piotr.nowakowski9@gmail.com>
2023-04-24 15:47:04 +0200

    Updated Font and ROLE

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,10 @@
+import { UserRole } from "./user-role";
+
 export class User {
     name?: string;
     lastName?: string;
     email?: string;
+    role?: UserRole;
     password?: string;
     passwordConfirm?: string;
 }
